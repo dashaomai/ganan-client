bb393e151458e623c7007a2e65fd428588321a30
Buddy.Jiang(江波达) <buddy.jiang@xsg.com.tw>
2022-01-09 15:48:46 +0800

    feat: 初步将连后端的代码调试完成

--- core/Bootstrap.cs
+++ core/Bootstrap.cs
@@ -4,6 +4,7 @@ using Godot;
 using GodotLogger;
 using Newtonsoft.Json.Linq;
 using PinusClient;
+using PinusClient.Transporter;
 using Rx.Net.Plus;
 
 public class Bootstrap : Node
@@ -30,6 +31,8 @@ public class Bootstrap : Node
         _client.OnNetworkStatusChanged += _OnNetworkStatusChanged;
         _client.OnHandshakeCompleted += _OnHandshakeCompleted;
 
+        _client.ConnectTo(new WebSocketParameter(_url));
+
         nickname = GetNode<Label>("MarginContainer/HBoxContainer/Nickname");
         avatar = GetNode<Label>("MarginContainer/HBoxContainer/Avatar");
         money = GetNode<Label>("MarginContainer/HBoxContainer/Money");
@@ -47,14 +50,22 @@ public class Bootstrap : Node
 
     private void _OnHandshakeCompleted(JObject obj)
     {
-        throw new NotImplementedException();
+        _log.Debug("handshake completed.");
+
+        var request = new {token = "buddy-01", vcode = "ganan-dev"};
+        _client.Request("connector.auth.logon", request, _OnLogon);
+    }
+
+    private void _OnLogon(JObject result)
+    {
+        _log.Debug($"logoned with response: {result}");
     }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
-        public override void _Process(float delta)
-        {
-            player.Wallet.Money.Value -= 1f;
-        }
+    public override void _Process(float delta)
+    {
+        player.Wallet.Money.Value -= 1f;
+    }
 
     private void _OnNetworkStatusChanged(NetworkStatus status)
     {
