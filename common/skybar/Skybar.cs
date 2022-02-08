98e96a2fb2cd459cf1c10f555ca29b14489ea7c1
Buddy(江波达) <dashaomai@qq.com>
2022-02-08 21:32:45 +0800

    feat: 简单地将 skybar 上的昵称及金币数与玩家默认钱包进行绑定

---
+++ common/skybar/Skybar.cs
@@ -0,0 +1,31 @@
+using Account;
+using Godot;
+using System;
+using Rx.Net.Plus;
+
+public class Skybar : MarginContainer
+{
+    private Player player;
+
+    private TextureRect avatar;
+    private Label nickname;
+    private Label coins;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        avatar = GetNode<TextureRect>("Row/H/Avatar");
+        nickname = GetNode<Label>("Row/H/V/Nickname");
+        coins = GetNode<Label>("Row/H/V/Coins");
+
+        player = GetNode<Player>("/root/Player");
+        player.Profile.Nickname.Notify(value => nickname.Text = value);
+        player.Wallets[109].Amount.Notify(value => coins.Text = value.ToString());
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
