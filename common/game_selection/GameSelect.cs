b6d5defd7da49c210d5877f9a28cb5f691730aee
Buddy(江波达) <dashaomai@qq.com>
2022-02-15 21:07:25 +0800

    feat: 为游戏选择场景，添加了动态跳转的功能

--- common/game_selection/GameSelect.cs
+++ common/game_selection/GameSelect.cs
@@ -1,8 +1,11 @@
 using Godot;
+using GodotLogger;
 using System;
 
 public class GameSelect : Node
 {
+    private static readonly Logger _log = LoggerHelper.GetLogger(typeof(GameSelect));
+
     private TextureButton mbj;
     private TextureButton txp;
 
@@ -11,6 +14,15 @@ public class GameSelect : Node
     {
         mbj = GetNode<TextureButton>("Layout/Icons/ItemList/MagicBlackJack");
         txp = GetNode<TextureButton>("Layout/Icons/ItemList/TexasPoker");
+
+        var mbjArgs = new Godot.Collections.Array();
+        mbjArgs.Add("mbj");
+
+        var txpArgs = new Godot.Collections.Array();
+        txpArgs.Add("txp");
+
+        mbj.Connect("pressed", this, nameof(_OnGameSelected), mbjArgs);
+        txp.Connect("pressed", this, nameof(_OnGameSelected), txpArgs);       
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +30,32 @@ public class GameSelect : Node
 //  {
 //      
 //  }
+
+    private void _OnGameSelected(string gameName)
+    {
+        string scenePath = null;
+
+        switch (gameName)
+        {
+            case "mbj":
+                scenePath = "res://games/blackjack/halls/halls.tscn";
+                break;
+
+            case "txp":
+                scenePath = "res://games/texasPoker/halls/halls.tscn";
+                break;
+        }
+
+        if (scenePath != null)
+        {
+            _log.Info($"change scene for game: {gameName} to: {scenePath}");
+
+            var scene = ResourceLoader.Load<PackedScene>(scenePath);
+            GetTree().ChangeSceneTo(scene);
+        }
+        else
+        {
+            _log.Warn($"cant change scene for un-support game: {gameName}");
+        }
+    }
 }
